549ee3b0f94a792d97983003804111fb5538acc7
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-09-27 15:13:15 +0530

    predict is added

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
-hepatitis=readtable('hepatitis_csv.csv');
+hepatitis_data=readtable('hepatitis_csv.csv');
+
+hepatitis=table2array(hepatitis_data)
 
 % training data
 training_class_name = hepatitis(1:110,20:20);
@@ -8,4 +10,12 @@ training_data = hepatitis(1:110,1:19);
 validating_class_names = hepatitis(111:142,20:20);
 validating_data = hepatitis(111:142,1:19);
 
-SVM_model=fitcsvm(data,class_name)
+% getting the svm model using fitcsvm function
+SVM_model=fitcsvm(training_data,training_class_name);
+
+% predicting the class names and also the score 
+[predicted_class_name,score] = predict(SVM_model,validating_data); 
+
+
+% checking the accuracy 
+accuracy = sum(predicted_class_name == validating_class_names)/numel(validating_class_names)
