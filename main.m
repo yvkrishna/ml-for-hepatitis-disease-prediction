64a62cf41929208f53a9fd6cdd2cf14655b3d9df
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-02 15:48:58 +0530

    Update main.m

--- main.m
+++ main.m
@@ -1,21 +1,32 @@
 hepatitis_data=readtable('hepatitis_2_csv.csv');
 
-hepatitis=table2array(hepatitis_data)
+hepatitis=table2array(hepatitis_data);
 
 % training data
-training_class_name = hepatitis(1:500,20:20);
-training_data = hepatitis(1:500,1:19);
+training_class_name = hepatitis(1:520,20:20);
+training_data = hepatitis(1:520,1:19);
 
 % validating data
-validating_class_names = hepatitis(501:568,20:20);
-validating_data = hepatitis(501:568,1:19);
+validating_class_names = hepatitis(521:568,20:20);
+validating_data = hepatitis(521:568,1:19);
+while 1==1
+    choice=menu('Pick One','        Training        ','       Testing        ','      exit      ');
+    if choice==1
+        % getting the svm model using fitcsvm function
+        SVM_model=fitcsvm(training_data,training_class_name);
+    else if choice==2
+        % predicting the class names and also the score 
+        [predicted_class_name,score] = predict(SVM_model,validating_data); 
 
-% getting the svm model using fitcsvm function
-SVM_model=fitcsvm(training_data,training_class_name);
-
-% predicting the class names and also the score 
-[predicted_class_name,score] = predict(SVM_model,validating_data); 
-
-
-% checking the accuracy 
-accuracy = sum(predicted_class_name == validating_class_names)/numel(validating_class_names)
+        % checking the accuracy 
+        accuracy = sum(predicted_class_name == validating_class_names)/numel(validating_class_names)
+        cm = confusionchart(validating_class_names,predicted_class_name);
+        mean_square_error=mean((validating_class_names-predicted_class_name).^2)
+        
+        else
+            close all;
+            break
+            clear;
+        end
+    end
+end
