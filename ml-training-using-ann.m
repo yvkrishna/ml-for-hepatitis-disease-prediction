b720e13deef876da6c9b2a4b38b5c25cff4ad9bb
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-13 16:29:04 +0530

    error removed
    removed the error which i was facing

--- ml-training-using-ann.m
+++ ml-training-using-ann.m
@@ -1,26 +1,25 @@
-  
 hepatitis_data=readtable('hepatitis_2_csv.csv');
 
 hepatitis=table2array(hepatitis_data);
 
 % Training Data
 training_class_name = hepatitis(1:520,20:20);
+training_class_name=transpose(training_class_name);
 training_data=hepatitis(1:520,1:19);
 
 % Validating Data
 validating_class_names = hepatitis(521:568,20:20);
+validating_class_names=transpose(validating_class_names);
 validating_data = hepatitis(521:568,1:19);
 
 % Input for ANN  
 tr1=transpose(training_data);
 theta1=randperm(10000,19);
 inputs1=theta1*tr1;
-inputs1=transpose(inputs1);
 
 tr2=transpose(validating_data);
 theta2=randperm(10000,19);
 inputs2=theta2*tr2;
-inputs2=transpose(inputs2);
 
 % Applying ANN
 net = feedforwardnet(10);
